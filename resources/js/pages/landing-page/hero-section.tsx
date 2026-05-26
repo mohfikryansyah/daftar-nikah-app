5061baa661815d505a4efd6d445d6df66abed4a3
mohfikryansyah <moh.fikryansyah@gmail.com>
2026-05-26 09:49:03 +0800

    feat: add current date to document templates in updatePuskesmas and updateKecamatan methods

--- resources/js/pages/landing-page/hero-section.tsx
+++ resources/js/pages/landing-page/hero-section.tsx
@@ -51,7 +51,7 @@ export default function HeroSection() {
                     className="relative z-10 mt-8 flex flex-wrap items-center justify-center gap-4"
                 >
                     <button
-                        onClick={() => router.get(route('catin.permohonan-nikah.index'))}
+                        onClick={() => router.get(route('register'))}
                         className="w-60 transform rounded-lg bg-yellow-400 px-6 py-2 font-medium text-gray-800 transition-all duration-300 hover:-translate-y-0.5 hover:bg-yellow-500 dark:bg-white dark:text-black dark:hover:bg-gray-200"
                     >
                         Daftar Sekarang
